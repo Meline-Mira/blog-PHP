3fb2f0536ce469a498b740fee0e7eb262a2813f0
Emeline Gineys <emeline.gineys@gmail.com>
2023-03-17 09:55:38 +0100

    start of the homepage

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require_once __DIR__.'/../views/home.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../views');
+$twig = new \Twig\Environment($loader, [
+    'cache' => __DIR__.'/../var/cache',
+    'debug' => true,
+]);
+$function = new \Twig\TwigFunction('urlIs', 'urlIs');
+$twig->addFunction($function);
+echo $twig->render('home.html.twig', ['page' => 'Accueil']);
--- views/home.php
+++
@@ -1,5 +0,0 @@
-<?php require_once __DIR__.'/layout/header.php' ?>
-
-ABC
-
-<?php require_once __DIR__.'/layout/footer.php' ?>
